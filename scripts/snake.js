01186c740bf91a226391011faab0159c84a5294e
Edward Reyes <reyes.edwardf@gmail.com>
2021-06-07 19:22:48 -0700

    changed dimesnions to 30X30 and middle position to 15x, 15y

--- scripts/snake.js
+++ scripts/snake.js
@@ -1,5 +1,5 @@
 export const SNAKE_SPEED = 2; // Snake will move 2 times per/sec
-const snakeBody = [{ x: 11, y: 11 }]; // position snake in the middle of the screen
+const snakeBody = [{ x: 15, y: 15 }]; // position snake in the middle of the screen
 
 export const update = () => {
   console.log('update snake')
