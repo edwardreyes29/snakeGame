e0bba8d8c913c9be68c0b3f12b75f3ca18c617fd
Edward Reyes <reyes.edwardf@gmail.com>
2021-06-07 19:06:11 -0700

    Render new frame twice per second

--- scripts/game.js
+++ scripts/game.js
@@ -2,19 +2,28 @@
   Set up Game loop 
 */
 let lastRenderTime = 0;
+const SNAKE_SPEED = 2; // Snake will move 2 times per/sec
+
 
 const main = currentTime => {
+
+  /* Requests a new frame 2 times per second */
+  window.requestAnimationFrame(main);
+
   // Get the seconds after the last render time.
   const secondsAfterLastRender = (currentTime - lastRenderTime) / 1000;
-  // Request a new frame
-  window.requestAnimationFrame(main);
+
+  // Check if seconds after the last render is less than the time between renders
+  if (secondsAfterLastRender < (1 / SNAKE_SPEED)) return;
+
+  console.log(secondsAfterLastRender);
+
   // Get the last render time
   lastRenderTime = currentTime;
-  console.log(secondsAfterLastRender);
 }
 
 /*
     Calls the main function to update an 
     animation before the next repaint.
 */
-// window.requestAnimationFrame(main);
+window.requestAnimationFrame(main);
